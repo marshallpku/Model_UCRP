482f00da057bc667e9c70b65b67937a4aae84e59
Yimeng Yin <Yimeng Yin>
2016-02-16 11:27:41 -0500

    add initial files from Model_Main

---
+++ FromMain/Install_Packages.R
@@ -0,0 +1,31 @@
+
+
+install.packages("dplyr")
+install.packages("ggplot2") 
+install.packages("plyr")
+install.packages("reshape2")
+install.packages("tidyr")
+
+install.packages("DataCombine")
+install.packages("gdata") # need to install perl: http://www.activestate.com/activeperl/
+
+install.packages("knitr")
+install.packages("rmarkdown")
+
+install.packages("zoo")
+
+install.packages("devtools")
+
+install.packages(c("magrittr",
+                   "data.table"))
+
+install.packages(c("foreach",
+                   "doParallel",
+                   "microbenchmark",
+                   "readxl",
+                   "stringr"
+                   ))
+
+install.packages("XLConnect")
+install.packages("xlsx")
+
